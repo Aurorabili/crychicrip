7751b45a1d8ca86eb72493e432cdaad43086574d
Aurorabili <aurorabili@outlook.com>
2025-02-17 14:43:05 +0000

    app
    Signed-off-by: Aurorabili <aurorabili@outlook.com>

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-white">
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+      <div class="fixed inset-0 -z-10 transform-gpu overflow-hidden blur-xl" aria-hidden="true"
+        style="background-image: url('bg2.png'); background-size: cover; background-position: center;">
+        <div class="relative" />
+      </div>
+      <!-- <div class="py-16" /> -->
+      <div class="relative z-10 flex flex-col items-center justify-center h-screen">
+        <div
+          class="fixed bottom-10 transform text-lg whitespace-nowrap flex flex-col items-center justify-center h-screen">
+          <p
+            class="text-2xl font-semibold tracking-tight text-balance text-blue-300 text-opacity-80 sm:text-5xl text-shadow-xl">
+            CRYCHICは
+            もうないんですのね
+          </p>
+          <p
+            class="text-2xl font-semibold tracking-tight text-balance text-blue-300 text-opacity-80 sm:text-5xl text-shadow-lg">
+            可是……CRYCHIC已经不复存在了啊
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+</script>
